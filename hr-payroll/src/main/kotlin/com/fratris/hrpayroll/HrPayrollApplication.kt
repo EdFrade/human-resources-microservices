28de9a679f97be70aae299deb143b1e19d3b0502
Eduardo <eduardofrad@hotmail.com>
2020-11-22 20:57:04 -0300

    feat: hr-payroll createad

---
+++ hr-payroll/src/main/kotlin/com/fratris/hrpayroll/HrPayrollApplication.kt
@@ -0,0 +1,11 @@
+package com.fratris.hrpayroll
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HrPayrollApplication
+
+fun main(args: Array<String>) {
+	runApplication<HrPayrollApplication>(*args)
+}
---
+++ hr-payroll/src/test/kotlin/com/fratris/hrpayroll/HrPayrollApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fratris.hrpayroll
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HrPayrollApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
